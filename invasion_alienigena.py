56702548e40146fb5463a8abf2109fe0ebc4da62
MeowXth <hacker7anony@gmail.com>
2022-05-03 16:46:46 -0400

    Agregando Funcionalidades

--- invasion_alienigena.py
+++ invasion_alienigena.py
@@ -3,7 +3,18 @@ import sys
 import pygame
 
 def run_game():
+    #inicializar el jeugo y crear un objeto en pantalla
     pygame.init()
     pantalla = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Invacion Alienigena")
-    
+    #iniciar el bucle principal del juego
+    while True:
+
+        #escuchar eventos de teclado o de raton
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        #hacer visible la pantalla dibujanda mas reciente        
+        pygame.display.flip()
+run_game()        
