5dac2a0d31bc77e864f54d02e7b8dfa21945b951
MeowXth <hacker7anony@gmail.com>
2022-05-03 16:40:39 -0400

    Avanzando codigo

--- invasion_alienigena.py
+++ invasion_alienigena.py
@@ -1,1 +1,9 @@
-print("chanchito feliz")
+import sys
+
+import pygame
+
+def run_game():
+    pygame.init()
+    pantalla = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Invacion Alienigena")
+    
